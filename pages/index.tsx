f9ee45d8faa7533c9ee3ab9e0ef201bc15eb03d6
Javier Gongora <jgongora@gmail.com>
2022-10-09 14:54:33 -0700

    Add default page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-red-200">Hello World!</div>;
+  return <div>Hello World!</div>;
 };
 
 export default Home;
