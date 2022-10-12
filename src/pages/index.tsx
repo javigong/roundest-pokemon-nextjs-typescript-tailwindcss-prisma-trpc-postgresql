b9ae4e3f03536e8564a57d445020ab16eaabe08e
Javier Gongora <jgongora@gmail.com>
2022-10-12 11:11:20 -0700

    add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
-import { trpc } from "@/utils/trpc";
-import type { NextPage } from "next";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 
-const Home: NextPage = () => {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Javier" }]);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (data) return <div>{data.greeting}</div>;
-
+const Home = () => {
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is roundest?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
