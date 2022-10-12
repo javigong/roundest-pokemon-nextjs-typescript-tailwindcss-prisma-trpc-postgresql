85c23c76cde3046a6f94bca1e62b091038b27017
Javier Gongora <jgongora@gmail.com>
2022-10-12 14:46:36 -0700

    Style buttons and fix trpc url gen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,20 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+function getBaseUrl() {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : `http://localhost:${process.env.PORT ?? 3000}/api/trpc`;
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
